1410bce6c91a874b9db80fae04ca50d617936da3
Dmanzella <58736612+Dmanzella@users.noreply.github.com>
2024-06-20 07:41:30 -0600

    Delete environemnt_setup.sh

--- environemnt_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# sets up your environment to spin up Malcolm VM using Vagrant, Ansible, and Virtualbox
-# not tested and probably is missing something
-
-sudo apt-get update && sudo apt install vagrant virtualbox pip ansible
-
-# pip install --include-deps ansible
